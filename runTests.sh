9f743c81ff1e9d24e5e2cd86d1c596e92649b330
xaninhpudins <alexandre.menino05@gmail.com>
2026-03-30 00:56:17 +0100

    FIX: IGDB Requests not working if file with token doesn't exist already; TEST: added script to run all tests on docker (linux only)

---
+++ runTests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker compose down -v ;
+
+docker compose up -d --build ;
+
+docker compose exec postgres sh -lc 'psql -U "$POSTGRES_USER" -d postgres -c "CREATE DATABASE test_db;" || true' ;
+
+docker compose exec server sh -lc "npx dotenv-cli -e .env.test -o -- npx prisma migrate deploy" ;
+
+docker compose exec server sh -lc "npm test" ;
+
+docker compose down -v ;
