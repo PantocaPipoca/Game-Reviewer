ebc18f77dfcac4883db82466113f2f9034fa9654
xaninhpudins <alexandre.menino05@gmail.com>
2026-04-20 15:17:09 +0100

    TEST: added fastCreate helper functions for each db table to use on tests

--- server/tests/integration/helper/helper.ts
+++ server/tests/integration/helper/helper.ts
@@ -4,6 +4,12 @@ import { StatusCodes } from "http-status-codes";
 import type { UserPK, AuthResponse, GameFull } from "../../../src/types/Types";
 import { PRISMA } from "../../../src/Prisma";
 import { AccountService } from "../../../src/services/AccountService";
+import { UserRepository } from "../../../src/Repository/UserRepository";
+import { GameRepository } from "../../../src/Repository/GameRepository";
+import { FollowerRepository } from "../../../src/Repository/FollowerRepository";
+import { ReviewRepository } from "../../../src/Repository/ReviewRepository";
+import { CommentRepository } from "../../../src/Repository/CommentRepository";
+import { LikeRepository } from "../../../src/Repository/LikeRepository";
 
 export async function register(
     app: Express,
@@ -49,5 +55,62 @@ export async function createGame(): Promise<GameFull> {
             gameName: "game_" + Date.now(),
             metadata: {},
         },
+    });
+}
+//
+//
+//
+//
+//
+// new fast functions to create db elements
+export function fastCreateUser(name: string) {
+    return UserRepository.insertUser({
+        accountName: name,
+        passwordHash: name,
+        avatar: null,
+        userData: {},
+        isPrivate: false,
+        email: `${name}@test.com`,
+    });
+}
+
+export function fastCreateFollower(follows: string, followed: string, accepted: boolean) {
+    return FollowerRepository.insertFollower({ follows, followed, accepted });
+}
+
+export function fastCreateGame(id: number) {
+    return GameRepository.insertGame({
+        gameID: id,
+        gameName: `${id}`,
+        metadata: {},
+    });
+}
+
+export function fastCreateReview(reviewer: string, reviewed: number) {
+    return ReviewRepository.insertReview({
+        reviewer,
+        reviewed,
+        text: `${reviewer},${reviewed}`,
+        score: 10,
+        hoursPlayed: null,
+        platforms: [],
+    });
+}
+
+export function fastCreateComment(reviewer: string, reviewed: number, commentator: string) {
+    return CommentRepository.insertComment({
+        reviewer,
+        reviewed,
+        text: `${reviewer},${reviewed},,${commentator}`,
+        commentator,
+    });
+}
+
+export function fastCreateLike(reviewer: string, reviewed: number, liker: string) {
+    return LikeRepository.insertLike({
+        reviewer,
+        reviewed,
+        value: true,
+        liker,
     });
 }
